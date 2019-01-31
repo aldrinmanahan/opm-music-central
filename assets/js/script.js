667e071bc7b0bd795510321ed7415328ec16eaa6
Aldrin Manahan <aldrinmanahan@gmail.com>
2019-01-31 16:46:50 +0800

    opm-music-central

---
+++ assets/js/script.js
@@ -0,0 +1,183 @@
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('hgalleryImg1');
+var artistNameText= document.getElementById("artistName");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt + " is a Filipino band formed in 1993. The band is known for its original rock novelty songs and often satirical covers of popular songs both foreign and local. The band is adept at playing in various musical genres.";
+    artistNameText.innerHTML = this.alt;
+}
+
+var img2 = document.getElementById('hgalleryImg2');
+var artistNameText= document.getElementById("artistName");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img2.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt + " is a rock band from the Philippines formed in 2008. The band had its beginnings as a supergroup with members from iconic Filipino bands, consisting of Franco Reyes (InYo) on vocals, Gabby Alipe (Urbandub) on guitar, Paolo Toleran (Queso) on guitar, Buwi Meneses (Parokya ni Edgar) on bass guitar, and JanJan Mendoza (Urbandub) on drums.";
+    artistNameText.innerHTML = this.alt;
+}
+
+var img3 = document.getElementById('hgalleryImg3');
+var artistNameText= document.getElementById("artistName");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img3.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt + " is a Filipino rock band formed in 2000. The band is composed of Jay Contreras (vocals), Jomal Linao and Led Tuyay on (Lead and Rhythm Guitar), Puto Astete (Bass) and Bords Burdeos (Drums).";
+    artistNameText.innerHTML = this.alt;
+}
+
+var img4 = document.getElementById('hgalleryImg4');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img4.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt + " is a rock band from Cebu City, Philippines. Since its inception in 2000, the band's line-up consisted of Gabby Alipe, John Dinopol, Lalay Lim, and JanJan Mendoza who joined in 2003. Urbandub is the first indie band in the Philippines to release albums nationwide with the assistance of a major label.";
+    artistNameText.innerHTML = this.alt;
+}
+
+var img5 = document.getElementById('hgalleryImg5');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img5.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt + " is a Filipino rock band formed in 1998. The band is composed of Yael Yuzon on vocals, Gosh Dilay on bass, Armo Armovit on guitars and drummer Tmac Cruz. One of the most popular rock bands in the Philippines, the band produced many hit songs including Jeepney, KLSP, Gemini, Bitiw, Tuliro, Di Na Mababawi, Tambay, Kay Tagal Kitang Hinintay, and XGF. To date, the band have released several albums that went successful."; 
+    artistNameText.innerHTML = this.alt;
+}
+
+var img6 = document.getElementById('hgalleryImg6');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img6.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt + " is a five-piece Filipino alternative rock band. The band is fronted by Monty Macalino, who due to his inarguably hefty body figure, makes him the most visible member of the group. The band is famous for winning the renowned 'Red Horse Muziklaban' in 2004.";
+    artistNameText.innerHTML = this.alt;
+}
+
+var img7 = document.getElementById('hgalleryImg7');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img7.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt + " is currently a 4-piece Filipino rock band that was formed in Metro Manila, Philippines in 2001. Five studio albums have been released by the band throughout its career.";
+    artistNameText.innerHTML = this.alt;
+}
+
+var img8 = document.getElementById('hgalleryImg8');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img8.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt + " was formed in 1999. They were signed to Sony BMG by 2007, and later to Ivory Records in 2012. Previously under Backbeat Management, they are now with Soupstar Management under Darwin Hernandez which also handles other bands including Sandwich, Callalily, 6cyclemind, Pedicab.";
+    artistNameText.innerHTML = this.alt;
+}
+
+var img9 = document.getElementById('hgalleryImg9');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img9.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt + " is an indie rock/jazz fusion band from the Philippines. The band is composed of Sud Ballecer on vocals and guitar, Jimbo Cuenco on drums, Carlos de la Fuente on saxophone, Kohl Aguilar on keyboard, and Raisa Racelis on bass. The band is popularly known for their hit singles 'Smilky', 'Profanities', 'Make U Say', and 'Sila'.";
+    artistNameText.innerHTML = this.alt;
+}
+
+var img10 = document.getElementById('hgalleryImg10');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img10.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt + " is an Alternative Rock band based in Manila, PH. Members consist of Zel Bautista, Jem Manuel, Don Gregorio, Jet Danao, and Gelo Cruz. They have released numerous singles and a self-titled debut album. Another album is on the works also.";
+    artistNameText.innerHTML = this.alt;
+}
+
+var img11 = document.getElementById('hgalleryImg11');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img11.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt + " is a Filipino synco-pop-punk-electro-rock band led by Eunice Jorge (vocals, piano, violin); Jazz Jorge (Bass), EJ Pichay (Drums) and Tatsi Jamnague (Guitars)";
+	artistNameText.innerHTML = this.alt;
+}
+
+var img12 = document.getElementById('hgalleryImg12');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img12.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt + " is a Filipino rock band formed in 2014. The original line-up consists of Unique Salonga on lead vocals and rhythm guitar, Zild Benitez on backing vocals and bass guitar, Blaster Silonga on lead guitar, and Badjao De Castro on drums. Salonga served as the band's lead vocalist and rhythm guitarist until 2018. The band are known for their 1970s-inspired outfits.";
+    artistNameText.innerHTML = this.alt;
+}
+
+var img13 = document.getElementById('hgalleryImg13');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img13.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt + " is an indie folk rock band in the Philippines. They consist of the brothers Paolo and Miguel Guico on acoustic guitars and vocals, Poch Barretto on electric guitar, Jam Villanueva on drums, Agnes Reoma on bass, Patricia Lasaten on keyboards, Toni Muñoz and Andrew de Pano on percussions, and Keifer Cabugao on violin.";
+    artistNameText.innerHTML = this.alt;
+}
+
+var img14 = document.getElementById('hgalleryImg14');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img14.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt + " is a singer and songwriter from the Philippines. Her debut album, Human Eyes, was independently released in March 2015. In November 2015, Clara released the EP titled Riverchild that she produced, wrote, and mixed.";
+    artistNameText.innerHTML = this.alt;
+}
+
+var img15 = document.getElementById('hgalleryImg15');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img15.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt + " is a Filipino musician, singer-songwriter, visual artist, graphic designer, fashion designer, and published author. She is best known for her 2015 debut album 'Arigato, Internet!', and for being half of the indie duo Reese and Vica.";
+    artistNameText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onscroll = function() {myFunction()};
+
+	var navbar = document.getElementById("navbar");
+	var sticky = navbar.offsetTop;
+
+	function myFunction() {
+	  if (window.pageYOffset >= sticky) {
+	    navbar.classList.add("sticky")
+	  } else {
+	    navbar.classList.remove("sticky");
+	  }
+	}
+
+
+function Sound(surl) {
+	document.getElementById("galleryImg1").innerHTML=
+	"<iframe src=\""+surl+"\" hidden=\"true\" autostart=\"true\" loop=\"false\" />";
+}
